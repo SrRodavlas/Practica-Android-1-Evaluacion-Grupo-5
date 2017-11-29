2d7d374059a452ed891ce630ac94fb3c60191ff8
DESKTOP-9OS1COE\Studium <informatic93@gmail.com>
2017-11-29 12:15:57 +0100

    Corrección con todos los archivos no subidos en el primer commit.

---
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/PrincipalVertical.java
@@ -0,0 +1,13 @@
+package com.grupo5.practica_android_1_evaluacion_grupo_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalVertical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_vertical);
+    }
+}
