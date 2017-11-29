78e3aaf55a95df702fa44d296e80117acdc12268
CarbonRuiner <actingasaparticle@gmail.com>
2017-11-29 13:48:09 +0100

    Creación de todas las activities verticales

--- app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/CitasVertical.java
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/CitasVertical.java
@@ -3,11 +3,11 @@ package com.grupo5.practica_android_1_evaluacion_grupo_5;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class CitasVertical extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_citas_vertical);
     }
 }
---
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/RegistroVertical.java
@@ -0,0 +1,13 @@
+package com.grupo5.practica_android_1_evaluacion_grupo_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroVertical extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_vertical);
+    }
+}
