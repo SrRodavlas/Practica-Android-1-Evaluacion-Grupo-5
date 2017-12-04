2cc67e30b10537368876f9973e32fdba6ab8e9dd
Studium(Salvador) <informatic93@gmail.com>
2017-12-04 13:04:46 +0100

    Implementación del código parcial de la activity 2.

--- app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Registro.java
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Registro.java
@@ -1,8 +1,10 @@
 package com.grupo5.practica_android_1_evaluacion_grupo_5;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 
 //https://developer.android.com/training/basics/firstapp/starting-activity.html
@@ -10,10 +12,10 @@ import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
 
-    /*public static final String nombrePorDefecto = "com.grupo5.practica_android_1_evaluacion_grupo_5.nombreUsuario";
+    public static final String nombrePorDefecto = "com.grupo5.practica_android_1_evaluacion_grupo_5.nombreUsuario";
 
     private EditText userName;
-    EditText nombreUsuario;*/
+    EditText nombreUsuario, email, contraseña;
 
 
     @Override
@@ -21,17 +23,25 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-
+        nombreUsuario = (EditText) findViewById(R.id.txtUserName);
+        email = (EditText) findViewById(R.id.txtEmail);
+        contraseña = (EditText) findViewById(R.id.txtPass);
 
     }
 
-    /*public void registrarse(){
-        Intent i = new Intent(this, Principal.class );
-       EditText ed = (EditText) findViewById(R.id.txtUserName);
+    public void registrarse(View v){
+        Intent intencion = new Intent(this, Principal.class );
+
+       /*EditText ed = (EditText) findViewById(R.id.txtUserName);
         String nombreUsuario = ed.getText().toString();
         getIntent().putExtra(nombrePorDefecto, nombreUsuario);
-        startActivityForResult(i,0);
-    }*/
+        startActivityForResult(i,0);*/
+    }
+    public void condiciones(View v){
+        Intent intencion = new Intent(Intent.ACTION_VIEW);
+        intencion.setData(Uri.parse("http://www.google.com"));
+        startActivity(intencion);
+    }
 
 
 
