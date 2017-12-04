a358f4d324c708230ef1bc2dd540a7735d615b6c
Studium(Salvador) <informatic93@gmail.com>
2017-12-04 23:32:03 +0100

    Mas parte de la implementación de la activity 3 y 4.

--- app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Citas.java
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Citas.java
@@ -2,12 +2,59 @@ package com.grupo5.practica_android_1_evaluacion_grupo_5;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class Citas extends AppCompatActivity {
+import com.grupo5.practica_android_1_evaluacion_grupo_5.citas.Cita;
+
+import java.util.ArrayList;
+
+public class Citas extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Spinner spiFecha, spiHora;
+    EditText mtxtMotivo;
+    ArrayList<Cita> lista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas);
+
+        spiFecha = (Spinner) findViewById(R.id.spiFecha);
+        ArrayAdapter<String> fechas = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.dias));
+        spiFecha.setAdapter(fechas);
+        spiFecha.setOnItemSelectedListener(this);
+        spiHora = (Spinner) findViewById(R.id.spiHora);
     }
 
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        ArrayAdapter<String> horas;
+        if(spiFecha.getItemAtPosition(position) == "15/12")
+        {
+            ArrayAdapter<String> horas = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    getResources().getStringArray(R.array.dias));
+
+        }
+        else if(spiFecha.getItemAtPosition(position) == "16/12")
+        {
+            iconoIdioma.setImageDrawable(getDrawable(R.drawable.fr));
+        }
+        else
+        {
+            iconoIdioma.setImageDrawable(getDrawable(R.drawable.en));
+        }
+        spiHora.setAdapter(fechas);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Menu.java
+++ app/src/main/java/com/grupo5/practica_android_1_evaluacion_grupo_5/Menu.java
@@ -3,8 +3,12 @@ package com.grupo5.practica_android_1_evaluacion_grupo_5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +26,9 @@ import java.util.ArrayList;
 public class Menu extends AppCompatActivity {
 
     TextView txtUsuario;
+    String nombreUsuario;
     ListView lvCitas;
+    EditText txtFecha, txtHora, txtConsulta;
     Button btnBorrar;
     ArrayList<Cita> citas;
 
@@ -35,39 +41,92 @@ public class Menu extends AppCompatActivity {
 
         Intent intencion = getIntent();
         if(intencion.getExtras() != null){
-            /*if(intencion.getExtras().getByteArray("lista") != null){
+            if(intencion.getExtras().getByteArray("lista") != null){
                 citas = conversorLista(intencion.getExtras().getByteArray("lista"));
-            }*/
+            }
 
         }
         if(citas != null) {
             citas = new ArrayList<Cita>();
         }
-        txtUsuario = (TextView) findViewById(R.id.txtvUsuario);
-        txtUsuario.setText(intencion.getExtras().getString("nombre"));
+        txtUsuario = (TextView) findViewById(R.id.txtNombreUsuario);
+        nombreUsuario = intencion.getExtras().getString("nombre");
+        txtUsuario.setText(nombreUsuario);
         lvCitas = (ListView) findViewById(R.id.lvCitas);
+        if(citas != null){
+            ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, adaptador(citas));
+            lvCitas.setAdapter(adaptador);
+        }
+        lvCitas.setClickable(true);
+        txtFecha = (EditText) findViewById(R.id.txtFecha);
+        txtHora = (EditText) findViewById(R.id.txtHora);
+        lvCitas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                //Object o = listView.getItemAtPosition(position);
+                // Realiza lo que deseas, al recibir clic en el elemento de tu listView determinado por su posicion.
+                Cita citaSeleccionada = null;
+                for(int x = 0; x < citas.size(); x++){
+                    if(lvCitas.getAdapter().getItem(x).equals(lvCitas.getSelectedItem())){
+                        citaSeleccionada = citas.get(x);
+                    }
+                }
+                txtFecha.setText(citaSeleccionada.obtenerDia());
+                txtHora.setText(citaSeleccionada.obtenerHora());
+                Log.i("Click", "click en el elemento " + position + " de mi ListView");
+
+            }
+        });
+
+        txtConsulta = (EditText) findViewById(R.id.txtConsulta);
+        txtConsulta.setText(Cita.CONSULTA);
+
+        Toast1 = Toast.makeText(getApplicationContext(), getString(R.string.msgSinCita), Toast.LENGTH_SHORT);
+        Toast1.setGravity(0,0,0);
     }
 
-    public void pedirCita(View v){
-        Intent intencion = new Intent(this, Citas.class );
-        intencion.putExtra("lista", conversorByte(citas));
-        startActivity(intencion);
-    }
-    public void borrarCita(View v){
-        if(lvCitas.isSelected()){
-
-        }
-        else{
-            Toast1 = Toast.makeText(getApplicationContext(), getString(R.string.msgSinCita), Toast.LENGTH_SHORT);
-            Toast1.setGravity(0,0,0);
-            Toast1.show();
-        }
-    }
     public void deconexion(View v){
         Intent intencion = new Intent(this, Principal.class );
         startActivity(intencion);
     }
-    public static byte[] conversorByte(ArrayList<Cita> lista){
+    public void pedirCita(View v){
+        Intent intencion = new Intent(this, Citas.class );
+        intencion.putExtra("lista", conversorByteLista(citas));
+        intencion.putExtra("nueva", true);
+        intencion.putExtra("nombre", nombreUsuario);
+        startActivity(intencion);
+    }
+    public void borrarCita(View v){
+        if(lvCitas.getSelectedItem() != null){
+            for(int x = 0; x < citas.size(); x++){
+                if(lvCitas.getAdapter().getItem(x).equals(lvCitas.getSelectedItem())){
+                    citas.remove(x);
+                }
+            }
+        }
+        else{
+            Toast1.show();
+        }
+    }
+    public void editarCita(View v){
+        if(lvCitas.isSelected()){
+            Intent intencion = new Intent(this, Citas.class );
+            intencion.putExtra("lista", conversorByteLista(citas));
+            intencion.putExtra("nueva", false);
+            intencion.putExtra("nombre", nombreUsuario);
+            //intencion.putExtra("cita", conversorByteCita(citas.get(lvCitas.get)));
+
+            startActivity(intencion);
+        }
+        else{
+            Toast1.show();
+        }
+    }
+
+    public static byte[] conversorByteLista(ArrayList<Cita> lista){
         byte[] objeto= null;
         try{
             ByteArrayOutputStream array = new ByteArrayOutputStream();
@@ -81,6 +140,13 @@ public class Menu extends AppCompatActivity {
             //Nada
         }
         return objeto;
+    }
+    private String[] adaptador(ArrayList<Cita> lista){
+        String[] elementos = new String[lista.size()];
+        for(int x = 0; x < lista.size(); x++){
+            elementos[x] = lista.get(x).obtenerDia() + " - " + lista.get(x).obtenerHora();
+        }
+        return elementos;
     }
     public static ArrayList<Cita> conversorLista(byte[] array){
         ArrayList<Cita> lista= null;
@@ -98,5 +164,37 @@ public class Menu extends AppCompatActivity {
             //Nada
         }
         return lista;
+    }
+    public static byte[] conversorByteCita(Cita cita){
+        byte[] objeto= null;
+        try{
+            ByteArrayOutputStream array = new ByteArrayOutputStream();
+            ObjectOutput salida = new ObjectOutputStream(array);
+            salida.writeObject(cita);
+            objeto = array.toByteArray();
+            array.close();
+            salida.close();
+        }
+        catch(IOException e){
+            //Nada
+        }
+        return objeto;
+    }
+    public static Cita conversorCita(byte[] array){
+        Cita cita= null;
+        try{
+            ByteArrayInputStream entrada = new ByteArrayInputStream(array);
+            ObjectInputStream objeto = new ObjectInputStream(entrada);
+            cita = (Cita) objeto.readObject();
+            entrada.close();
+            objeto.close();
+        }
+        catch (IOException e){
+            //Nada
+        }
+        catch (ClassNotFoundException cnfe){
+            //Nada
+        }
+        return cita;
     }
 }
